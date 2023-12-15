a4c21857837da200e52d67ad8869b3148790e956
SechabaMadyibi <sechabamadyibi1@gmail.com>
2023-12-15 20:47:07 +0200

    first commit

---
+++ 06-ObjectsClasses/Practices/Practice1/06-ObjectsClasses_Practice1/src/soccer/Game.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer;
+
+/**
+ *
+ * @author dmtshoeni
+ */
+public class Game {
+    public Team home;
+    public Team away;
+    public Goal goals;
+}
